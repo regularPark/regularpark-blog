3c97f07714fad8a8ad31947698d749afbba0f748
regularPark <srk99279@naver.com>
2023-12-08 16:16:31 +0900

    chore: mdx 포스팅 환경설정

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,5 +1,25 @@
-const PostList = () => {
-  return <div>PostList</div>;
+import PostItem from './Post/PostItem';
+
+export type PostItemType = {
+  title: string;
+  // categories: string[];
+  date: string;
+  // description: string;
+};
+
+interface PostListProps {
+  posts: PostItemType[];
+}
+
+const PostList = ({ posts }: PostListProps) => {
+  posts.sort((a, b) => (a.date < b.date ? -1 : 1));
+  return (
+    <div className="grid grid-cols-2 gap-4 mx-auto py-24">
+      {posts.map(post => (
+        <PostItem post={post} />
+      ))}
+    </div>
+  );
 };
 
 export default PostList;
