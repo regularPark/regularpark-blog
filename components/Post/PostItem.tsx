8fb9a1c59d971704aa90dcbd2583ec94b3bbfb8b
regularPark <srk99279@naver.com>
2023-12-11 18:38:24 +0900

    feat: `useMdxComponent`를 이용해 mdx파일을 렌더링

--- components/Post/PostItem.tsx
+++ components/Post/PostItem.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
 import { PostItemType } from '../PostList';
+import Link from 'next/link';
 
 interface PostItemProps {
   post: PostItemType;
@@ -6,10 +8,41 @@ interface PostItemProps {
 
 const PostItem = ({ post }: PostItemProps) => {
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.date}</p>
-    </div>
+    <Link
+      href={post.url}
+      className="flex flex-col rounded-lg shadow-md transition-all hover:shadow-[0_0_20px_4px_rgba(0,0,0,0.2)] cursor-pointer"
+    >
+      {post.thumbnail && (
+        <Image
+          className="w-full h-48 rounded-t-lg"
+          src={post.thumbnail}
+          width={120}
+          height={80}
+          alt="hi"
+        />
+      )}
+      <div className="flex flex-col flex-grow p-4">
+        <p className="text-sm font-light text-opacity-70">
+          {post.date.slice(0, 10).replaceAll('-', '.')}
+        </p>
+        <h2 className="mb-1 overflow-hidden text-lg font-bold leading-tight">
+          {post.title}
+        </h2>
+        <div className="flex flex-wrap mt-2 -m-1">
+          {post.categories.split(',').map(item => (
+            <div
+              key={item}
+              className="m-1 px-2 py-1 rounded text-sm font-bold text-white bg-black"
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+        <p className="mt-auto overflow-hidden text-base text-opacity-80">
+          {post.summary}
+        </p>
+      </div>
+    </Link>
   );
 };
 
--- components/PostList.tsx
+++ components/PostList.tsx
@@ -2,9 +2,11 @@ import PostItem from './Post/PostItem';
 
 export type PostItemType = {
   title: string;
-  // categories: string[];
+  categories: string;
   date: string;
-  // description: string;
+  thumbnail: string;
+  summary: string;
+  url: string;
 };
 
 interface PostListProps {
@@ -13,8 +15,9 @@ interface PostListProps {
 
 const PostList = ({ posts }: PostListProps) => {
   posts.sort((a, b) => (a.date < b.date ? -1 : 1));
+
   return (
-    <div className="grid grid-cols-2 gap-4 mx-auto py-24">
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full mx-auto py-12 px-4 sm:px-6 lg:max-w-5xl lg:px-8 lg:py-24">
       {posts.map(post => (
         <PostItem post={post} />
       ))}
