255ca7b3cb4c4e5494279429cbb66ca2c38b3f40
regularPark <srk99279@naver.com>
2023-12-12 22:13:29 +0900

    feat: `BackButton` 적용 및 스타일 수정

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from 'next/navigation';
 import type { MDXComponents } from 'mdx/types';
 import Link from 'next/link';
 import { useMDXComponent } from 'next-contentlayer/hooks';
+import BackButton from '@/components/common/BackButton';
 
 const mdxComponents: MDXComponents = {
   a: ({ href, children }) => <Link href={href as string}>{children}</Link>,
@@ -24,14 +25,25 @@ export default function Page({ params }: { params: { slug: string } }) {
   const MDXContent = useMDXComponent(post.body.code);
 
   return (
-    <article className="mx-auto prose">
-      <div className="mb-8 text-center">
-        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
-          {new Intl.DateTimeFormat('ko-KR').format(new Date(post.date))}
-        </time>
-        <h1 className="text-3xl font-bold">{post.title}</h1>
-      </div>
-      <MDXContent components={mdxComponents} />
-    </article>
+    <>
+      <BackButton />
+      <img
+        src={post.thumbnail}
+        className="w-screen h-80 object-cover opacity-70 bg-gray-300"
+        alt="hi"
+      />
+      <article className="mx-auto prose">
+        <div className="my-8 text-center">
+          <time
+            dateTime={post.date}
+            className="mt-3 mb-1 text-xs text-gray-600"
+          >
+            {new Intl.DateTimeFormat('ko-KR').format(new Date(post.date))}
+          </time>
+          <h1 className="text-3xl font-bold">{post.title}</h1>
+        </div>
+        <MDXContent components={mdxComponents} />
+      </article>
+    </>
   );
 }
