cde4e0048b7da35a7e39d4774dc10974ad54c58c
regularPark <srk99279@naver.com>
2023-12-21 01:59:53 +0900

    chore: google analytics 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="QXCt1rHR8GlnfcaX2CeaMmTAyjZnHME4405PEA_aZHE"
+        />
+      </head>
       <body className={inter.className}>
         <ToggleDarkMode />
         {children}
