2828def77cb32dd59384dc2950c8a10ac2eff8e4
regularPark <srk99279@naver.com>
2024-01-15 21:56:59 +0900

    refactor: 댓글 테마 적용

--- app/Giscus.tsx
+++ app/Giscus.tsx
@@ -1,13 +1,11 @@
 'use client';
-import { useTheme } from 'next-themes';
 import { useEffect, useRef } from 'react';
 
 export default function Giscus() {
   const ref = useRef<HTMLDivElement>(null);
-  const { resolvedTheme } = useTheme();
 
   // https://github.com/giscus/giscus/tree/main/styles/themes
-  const theme = resolvedTheme === 'dark' ? 'dark' : 'light';
+  const theme = localStorage.getItem('theme') ?? 'dark';
 
   useEffect(() => {
     if (!ref.current || ref.current.hasChildNodes()) return;
@@ -26,7 +24,7 @@ export default function Giscus() {
     scriptElem.setAttribute('data-reactions-enabled', '1');
     scriptElem.setAttribute('data-emit-metadata', '0');
     scriptElem.setAttribute('data-input-position', 'bottom');
-    scriptElem.setAttribute('data-theme', 'dark');
+    scriptElem.setAttribute('data-theme', theme);
     scriptElem.setAttribute('data-lang', 'en');
 
     ref.current.appendChild(scriptElem);
